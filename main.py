cfa186c664c3aa9d5f1ecdab2a1c0d2ceea35768
Vitor Rodrigo <120431996+VitorRSBonfim@users.noreply.github.com>
2025-05-02 19:32:58 -0300

    _init

---
+++ main.py
@@ -0,0 +1,30 @@
+
+# Para realizar "operações" em um arquivo json é preciso utilizar 'import json'
+import json
+#Pegar json da solicitação do cliente
+# Pegar Banco de dados separado de SKUs
+# Criar SKU desejado pelo cliente, acima do id único +
+# Verificar se o sku não existe no sistema
+# Entregar SKU e adicionar SKU no sistema
+print("Hello, Wolrd!!")
+
+# Carregar json dos 10 últimos sku criados
+
+# Teste arquivo JSON (Java Script Object Notation)
+
+# Estrutura JSON SKU
+
+# Criar SKC com base no id único SKC
+
+# Estrutura SKC sc + 020525 + 0000000001 + 0000000001  
+# Estrutura SKU 00000000014d42
+# Adicionar 000 ao id, se o ID for 1 adicionar 9 zeros ao início do ID ID deverá ser adicionado na criação do SKC em uma linha separada de forma que seja possível comparar o ID recém criado com o id original (sem adição dos 0)
+#Modelo DB ----> id | nome | dt de criação | token criador | sku | skc | id skc | id cor
+
+sku_JSON = '{"id" : id, id_comparador : id_comparador ,nome : "nome", dt_cr : dt_cr, token_cr : "token_cr", }'
+
+x = '{"marca" : "nissan"}'
+
+y = json.loads(x)
+
+print("A marca é " + y["marca"])
